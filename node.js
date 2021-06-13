0c204d572976eb16248f18ed0ee5bcf8b0f2a27d
Sufyan <khotsufyan@gmail.com>
2021-06-13 13:34:02 +0530

    Doubly linked lists added

--- node.js
+++ node.js
@@ -137,21 +137,128 @@ class SinglyLinkedList {
     }
 }
 
+class DoublyLinkedList {
+    constructor (value=null) {
+        let headNode = new Node(value, null, null);
+        this.head = headNode;
+        this.tail = headNode;
+    }
 
-let singlyLL = new SinglyLinkedList(3);
-singlyLL.addNode(7);
-singlyLL.addNode(10);
-singlyLL.addNode(13);
-singlyLL.addToHead(19);
-singlyLL.addAfterNode(7, 8);
-singlyLL.addAfterNode(13,14);
-singlyLL.addAfterNode(19,20);
-console.log(singlyLL.toArray());
-singlyLL.deleteNode(13);
-console.log(singlyLL.toArray());
-singlyLL.deleteNode(19);
-console.log(singlyLL.toArray());
-singlyLL.deleteNode(14);
-console.log(singlyLL.toArray());
-console.log(singlyLL.getHead());
-console.log(singlyLL.getTail());
+    addNode(value=null) {
+        let endNode = new Node(value, this.tail, null);
+        this.tail.next = endNode;
+        this.tail = endNode;
+    }
+
+    isEmpty() {
+        if (this.head == null) return true;
+        return false; 
+    }
+
+    getHead() {
+        return this.head;
+    }
+
+    getTail() {
+        return this.tail;
+    }
+
+    toArray() {
+        //shows all the nodes in sequence
+        let valuesList = [];
+        let tailReached = false;
+        let currentNode = this.head;
+        while(!tailReached) {
+            valuesList.push(currentNode.value);
+            if (currentNode === this.tail) {
+                tailReached = true;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        return valuesList;
+    }
+    printReverse() {
+        //shows all the nodes in reverse order
+        let currentNode = this.tail;
+        let headReached = false;
+        let reverseArray = [];
+        while (!headReached) {
+            reverseArray.push(currentNode.value);
+            if (currentNode === this.head) {
+                headReached = true;
+            }
+            currentNode = currentNode.prev;
+        }
+        return reverseArray;
+    }
+
+    addToHead(value) {
+        let newNode = new Node(value, null, this.head);
+        this.head.prev = newNode;
+        this.head = newNode;
+    }
+
+    addAfterNode(searchValue, value=null) {
+        if (this.tail.value === searchValue) {
+            this.addNode(value);
+        } else {
+            let tailReached = false;
+            let currentNode = this.head;
+            while(!tailReached) {
+                if (currentNode.value === searchValue) {
+                    let nextNode = currentNode.next;
+                    let newNode = new Node(value, currentNode, nextNode);
+                    // newNode.next = nextNode;
+                    currentNode.next = newNode;
+                    nextNode.prev = newNode;
+                    break;
+                }
+                if (currentNode === this.tail) {
+                    tailReached = true;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+            if (tailReached) {
+                console.log("The given value could not be found in the list");
+            }
+        }
+    }
+
+    deleteNode(searchValue) {
+        if (this.head.value === searchValue) {
+            this.head = this.head.next; //updating head node
+        } else if (this.tail.value === searchValue) {
+            this.tail = this.tail.prev; //set previous node as tail
+        } else {
+            let currentNode = this.head.next; //starting from 2nd node
+            let tailReached = false;
+            while (!tailReached) {
+                if (currentNode.value === searchValue) {
+                    currentNode.prev.next = currentNode.next;
+                    currentNode.next.prev = currentNode.prev;
+                }
+                if (this.tail === currentNode) {
+                    tailReached = true;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+    }
+}
+
+let dll = new DoublyLinkedList(1);
+dll.addNode(2);
+dll.addNode(3);
+dll.addToHead(4);
+dll.addToHead(5);
+dll.addAfterNode(3, 11);
+dll.addAfterNode(4, 12);
+console.log(dll.toArray());
+console.log(dll.printReverse());
+dll.deleteNode(4);
+dll.deleteNode(11);
+dll.deleteNode(2);
+console.log(dll.toArray());
+console.log(dll.printReverse());

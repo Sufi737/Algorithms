b5c5f9d53fddd3f95ab28df714151cbacad57578
Sufyan <khotsufyan@gmail.com>
2021-06-22 21:13:21 +0530

    Priority queue implementation added

--- node.js
+++ node.js
@@ -1,264 +1,145 @@
-class Node {
-    constructor(value=null, prev=null, next=null) {
-        this.value = value;
-        this.prev = prev;
-        this.next = next;
+class PriorityQueue {
+    constructor (size) {
+        this.maxSize = size === null ? 1 : size;
+        this.queue = [];
     }
 
-    getValue() {
-        return this.value;
-    }
-
-    getNextNode() {
-        return this.next;
-    }
-
-    getPrevNode() {
-        return this.prev;
-    }
-}
-
-class SinglyLinkedList {
-    constructor(value = null) {
-        this.head = new Node(value, null, null); //setting the first node as head node
-        this.tail = this.head;
-    }
-
-    isEmpty() {
-        if (this.head == null) return true;
-        return false; 
-    }
-
-    addNode(value = null) {
-        let lastNode = new Node(value, null, null);
-        this.tail.next = lastNode;
-        this.tail = lastNode;
-    }
-
-    addToHead(value = null) {
-        let newNode = new Node(value, null, this.head);
-        this.head = newNode;
-    }
-
-    addAfterNode(searchValue, value=null) {
-        if (this.tail.value === searchValue) {
-            this.addNode(value);
+    add (value) {
+        if (this.queue.length >= this.maxSize) {
+            console.log("Queue max size reached");
+            return false;
         } else {
-            let tailReached = false;
-            let currentNode = this.head;
-            while(!tailReached) {
-                if (currentNode.value === searchValue) {
-                    let nextNode = currentNode.next;
-                    let newNode = new Node(value, null, nextNode);
-                    newNode.next = nextNode;
-                    currentNode.next = newNode;
-                    break;
-                }
-                if (currentNode === this.tail) {
-                    tailReached = true;
-                } else {
-                    currentNode = currentNode.next;
+            this.queue.push(value);
+            let currentIndex = this.queue.length-1;
+            //check if this is the root node
+            if (this.queue.length !== 1) {
+                let parent = this.getParentNode(currentIndex);
+                //check for min heap property
+                if (value < this.queue[parent]) {
+                    //heap invalid
+                    this.swim(parent, currentIndex);
                 }
             }
-            if (tailReached) {
-                console.log("The given value could not be found in the list");
-            }
+            return true;
         }
     }
 
-    deleteNode(searchValue) {
-        if (this.head.value === searchValue) {
-            //update head
-            let newHead = this.head.next;
-            this.head = newHead;
-            console.log("Head updated!");
+    swim (parentIndex, childIndex) {
+        //swap the nodes
+        let temp = this.queue[childIndex];
+        this.queue[childIndex] = this.queue[parentIndex];
+        this.queue[parentIndex] = temp;
+        if (parentIndex === 0) {
+            //root node swapped, stop here
+            return true;
         } else {
-            //keeping 2 pointers to keep track of previous node
-            let currentNode = this.head.next;
-            let nextNode = this.head.next.next;
-            while (true) {
-                if (currentNode == null) {
-                    console.log("Value not found");
-                    return;
-                } else if (nextNode.value === null) {
-                    if (currentNode.value === searchValue) {
-                        //this is the tail node and we need to update the tail
-                        currentNode.next = null;
-                        this.tail = currentNode;
-                        console.log("Tail updated!");
+            //check for min heap after swap 
+            childIndex = parentIndex;
+            parentIndex = this.getParentNode(parentIndex);
+            if (this.queue[childIndex] < this.queue[parentIndex]) {
+                this.swim(parentIndex, childIndex)
+            }
+            return true;
+        }
+    }
+
+    getParentNode (childIndex) {
+        // returns the index of teh parent node
+        if (childIndex === 0) {
+            console.log("Dont pass root node");
+            return false;
+        }
+        let parentIndex = 0;
+        if (childIndex%2 !== 0) {
+            parentIndex = Math.floor(childIndex/2);
+        } else {
+            parentIndex = Math.floor((childIndex-1)/2);
+        }
+        return parentIndex;
+    }
+
+    delete (value) {
+        for (let i=0; i<this.queue.length;i++) {
+            if (this.queue[i] === value) {
+                //replace this with the last node
+                this.queue[i] = this.queue[this.queue.length-1];
+                this.queue.pop(); //removing the last node
+                //check if heap is valid
+                let leftChild = 2*i+1;
+                let rightChild = 2*i+2;
+                if (leftChild >= this.queue.length) {
+                    leftChild = null;
+                }
+                if (rightChild >= this.queue.length) {
+                    rightChild = null;
+                }
+                //find out which child to compare with, if any
+                let childToCompare = null;
+                if (leftChild !== null && rightChild !== null) {
+                    if (this.queue[leftChild] < this.queue[rightChild]) {
+                        childToCompare = leftChild;
                     } else {
-                        console.log("Value not found");
+                        childToCompare = rightChild;
                     }
-                    return; 
+                } else if (leftChild === null && rightChild !== null) {
+                    childToCompare = rightChild;
+                } else if (leftChild !== null && rightChild === null) {
+                    childToCompare = leftChild;
                 } else {
-                    if (nextNode.value === searchValue) {
-                        if (this.tail === nextNode) {
-                            //update the tail
-                            currentNode.next = null;
-                            this.tail = currentNode;
-                            console.log("Tail updated!");
-                        } else {
-                            let newNext = nextNode.next;
-                            currentNode.next = newNext;
-                            console.log("Node removed");
-                        }
-                        return;
-                    } else {
-                        currentNode = nextNode;
-                        nextNode = nextNode.next;
-                    }
+                    //both are null means no child node
+                    return true;
                 }
+                //compare with the child node
+                if (this.queue[childToCompare] < this.queue[i]) {
+                    //sink
+                    this.sink(i, childToCompare);
+                }
+                return true;
             }
         }
+        console.log("Node with give value not found");
     }
 
-    getHead() {
-        return this.head;
+    sink (parentIndex, childIndex) {
+        let temp = this.queue[parentIndex];
+        this.queue[parentIndex] = this.queue[childIndex];
+        this.queue[childIndex] = temp;
+
+        parentIndex = childIndex;
+         //check if heap is valid
+         let leftChild = 2*parentIndex+1;
+         let rightChild = 2*parentIndex+2;
+         if (leftChild >= this.queue.length) {
+             leftChild = null;
+         }
+         if (rightChild >= this.queue.length) {
+             rightChild = null;
+         }
+         //find out which child to compare with, if any
+         let childToCompare = null;
+         if (leftChild !== null && rightChild !== null) {
+             if (this.queue[leftChild] < this.queue[rightChild]) {
+                 childToCompare = leftChild;
+             } else {
+                 childToCompare = rightChild;
+             }
+         } else if (leftChild === null && rightChild !== null) {
+             childToCompare = rightChild;
+         } else if (leftChild !== null && rightChild === null) {
+             childToCompare = leftChild;
+         } else {
+             //both are null means no child node
+             return true;
+         }
+         //compare with the child node
+         if (this.queue[childToCompare] < this.queue[parentIndex]) {
+             //sink
+             this.sink (parentIndex, childToCompare);
+         }
+         return true;
     }
 
-    getTail() {
-        return this.tail;
-    }
-
-    toArray() {
-        //shows all the nodes in sequence
-        let valuesList = [];
-        let tailReached = false;
-        let currentNode = this.head;
-        while(!tailReached) {
-            valuesList.push(currentNode.value);
-            if (currentNode === this.tail) {
-                tailReached = true;
-            } else {
-                currentNode = currentNode.next;
-            }
-        }
-        return valuesList;
+    dequeue () {
+        this.delete(this.queue[0]); // just remove the root node
     }
 }
-
-class DoublyLinkedList {
-    constructor (value=null) {
-        let headNode = new Node(value, null, null);
-        this.head = headNode;
-        this.tail = headNode;
-    }
-
-    addNode(value=null) {
-        let endNode = new Node(value, this.tail, null);
-        this.tail.next = endNode;
-        this.tail = endNode;
-    }
-
-    isEmpty() {
-        if (this.head == null) return true;
-        return false; 
-    }
-
-    getHead() {
-        return this.head;
-    }
-
-    getTail() {
-        return this.tail;
-    }
-
-    toArray() {
-        //shows all the nodes in sequence
-        let valuesList = [];
-        let tailReached = false;
-        let currentNode = this.head;
-        while(!tailReached) {
-            valuesList.push(currentNode.value);
-            if (currentNode === this.tail) {
-                tailReached = true;
-            } else {
-                currentNode = currentNode.next;
-            }
-        }
-        return valuesList;
-    }
-    printReverse() {
-        //shows all the nodes in reverse order
-        let currentNode = this.tail;
-        let headReached = false;
-        let reverseArray = [];
-        while (!headReached) {
-            reverseArray.push(currentNode.value);
-            if (currentNode === this.head) {
-                headReached = true;
-            }
-            currentNode = currentNode.prev;
-        }
-        return reverseArray;
-    }
-
-    addToHead(value) {
-        let newNode = new Node(value, null, this.head);
-        this.head.prev = newNode;
-        this.head = newNode;
-    }
-
-    addAfterNode(searchValue, value=null) {
-        if (this.tail.value === searchValue) {
-            this.addNode(value);
-        } else {
-            let tailReached = false;
-            let currentNode = this.head;
-            while(!tailReached) {
-                if (currentNode.value === searchValue) {
-                    let nextNode = currentNode.next;
-                    let newNode = new Node(value, currentNode, nextNode);
-                    // newNode.next = nextNode;
-                    currentNode.next = newNode;
-                    nextNode.prev = newNode;
-                    break;
-                }
-                if (currentNode === this.tail) {
-                    tailReached = true;
-                } else {
-                    currentNode = currentNode.next;
-                }
-            }
-            if (tailReached) {
-                console.log("The given value could not be found in the list");
-            }
-        }
-    }
-
-    deleteNode(searchValue) {
-        if (this.head.value === searchValue) {
-            this.head = this.head.next; //updating head node
-        } else if (this.tail.value === searchValue) {
-            this.tail = this.tail.prev; //set previous node as tail
-        } else {
-            let currentNode = this.head.next; //starting from 2nd node
-            let tailReached = false;
-            while (!tailReached) {
-                if (currentNode.value === searchValue) {
-                    currentNode.prev.next = currentNode.next;
-                    currentNode.next.prev = currentNode.prev;
-                }
-                if (this.tail === currentNode) {
-                    tailReached = true;
-                }
-                currentNode = currentNode.next;
-            }
-        }
-    }
-}
-
-let dll = new DoublyLinkedList(1);
-dll.addNode(2);
-dll.addNode(3);
-dll.addToHead(4);
-dll.addToHead(5);
-dll.addAfterNode(3, 11);
-dll.addAfterNode(4, 12);
-console.log(dll.toArray());
-console.log(dll.printReverse());
-dll.deleteNode(4);
-dll.deleteNode(11);
-dll.deleteNode(2);
-console.log(dll.toArray());
-console.log(dll.printReverse());

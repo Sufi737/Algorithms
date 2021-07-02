502b9727febff5f31b6178cc78e8124f04fd4162
Sufyan <khotsufyan@gmail.com>
2021-07-02 21:07:23 +0530

    Binary search trees remove node

--- node.js
+++ node.js
@@ -1,145 +1,91 @@
-class PriorityQueue {
-    constructor (size) {
-        this.maxSize = size === null ? 1 : size;
-        this.queue = [];
+function TreeNode (value, left, right) {
+    this.value = value===null ? null : value;
+    this.left = left===null ? null : left;
+    this.right = right===null ? null : right;
+}
+
+
+class BinarySearchTree {
+    constructor (value, left, right) {
+        this.root = new TreeNode(value, left, right)
+        this.nodesCount = value===null ? 0 : 1;
     }
 
     add (value) {
-        if (this.queue.length >= this.maxSize) {
-            console.log("Queue max size reached");
-            return false;
-        } else {
-            this.queue.push(value);
-            let currentIndex = this.queue.length-1;
-            //check if this is the root node
-            if (this.queue.length !== 1) {
-                let parent = this.getParentNode(currentIndex);
-                //check for min heap property
-                if (value < this.queue[parent]) {
-                    //heap invalid
-                    this.swim(parent, currentIndex);
-                }
-            }
+        //if this is the first node
+        if (this.root === null) {
+            this.root = new TreeNode(value, null, null)
+            this.nodesCount++;
             return true;
         }
+        add (this.root, value)
     }
 
-    swim (parentIndex, childIndex) {
-        //swap the nodes
-        let temp = this.queue[childIndex];
-        this.queue[childIndex] = this.queue[parentIndex];
-        this.queue[parentIndex] = temp;
-        if (parentIndex === 0) {
-            //root node swapped, stop here
+    add (parent, value) {
+        //traverses the tree till leaf is met
+        if (parent.left === null && value < parent.value) {
+            parent.left = new TreeNode(value, null, null)
+            return true;
+        } else if (parent.right === null && parent.value < value) {
+            parent.right = new TreeNode(value, null, null)
             return true;
         } else {
-            //check for min heap after swap 
-            childIndex = parentIndex;
-            parentIndex = this.getParentNode(parentIndex);
-            if (this.queue[childIndex] < this.queue[parentIndex]) {
-                this.swim(parentIndex, childIndex)
+            if (parent.value < value) {
+                parent = parent.right;
+            } else {
+                parent = parent.left;
             }
-            return true;
+            add (parent, value)
         }
     }
 
-    getParentNode (childIndex) {
-        // returns the index of teh parent node
-        if (childIndex === 0) {
-            console.log("Dont pass root node");
-            return false;
+    find (value) {
+        let start = this.root
+        while (start) {
+            if (start.value === value) {
+                return start;
+            } else if (start.value < value) {
+                start = start.right
+            } else {
+                start = start.left
+            }
         }
-        let parentIndex = 0;
-        if (childIndex%2 !== 0) {
-            parentIndex = Math.floor(childIndex/2);
+        return false;
+    }
+
+    remove (value) {
+        let nodeToDelete = this.find(value);
+        if (!nodeToDelete) {
+            console.log("Given node not found in the tree")
+            return false
+        } 
+        if (nodeToDelete.left === null && nodeToDelete.right === null) {
+            //leaf node 
+            nodeToDelete.value = null
+        } else if (nodeToDelete.left === null) {
+            let leftSubtree = nodeToDelete.left
+            //swap the value of the node to delete with its left child
+            nodeToDelete.value = leftSubtree.value
+        } else if (nodeToDelete.right === null) {
+            let rightSubtree = nodeToDelete.right
+            nodeToDelete.value = rightSubtree.value
         } else {
-            parentIndex = Math.floor((childIndex-1)/2);
+            //both subtrees present
+            this.root = nodeToDelete.left;
+            //adding the right subtree to the rightmost child of the left subtree
+            let rightSubtree = nodeToDelete.right
+            let rightParent = this.findRightLeaf()
+            rightParent.right = rightSubtree
         }
-        return parentIndex;
     }
 
-    delete (value) {
-        for (let i=0; i<this.queue.length;i++) {
-            if (this.queue[i] === value) {
-                //replace this with the last node
-                this.queue[i] = this.queue[this.queue.length-1];
-                this.queue.pop(); //removing the last node
-                //check if heap is valid
-                let leftChild = 2*i+1;
-                let rightChild = 2*i+2;
-                if (leftChild >= this.queue.length) {
-                    leftChild = null;
-                }
-                if (rightChild >= this.queue.length) {
-                    rightChild = null;
-                }
-                //find out which child to compare with, if any
-                let childToCompare = null;
-                if (leftChild !== null && rightChild !== null) {
-                    if (this.queue[leftChild] < this.queue[rightChild]) {
-                        childToCompare = leftChild;
-                    } else {
-                        childToCompare = rightChild;
-                    }
-                } else if (leftChild === null && rightChild !== null) {
-                    childToCompare = rightChild;
-                } else if (leftChild !== null && rightChild === null) {
-                    childToCompare = leftChild;
-                } else {
-                    //both are null means no child node
-                    return true;
-                }
-                //compare with the child node
-                if (this.queue[childToCompare] < this.queue[i]) {
-                    //sink
-                    this.sink(i, childToCompare);
-                }
-                return true;
-            }
+    findRightLeaf () {
+        let start = this.root
+        while (start) {
+           if (start.right === null) {
+               return start
+           }
         }
-        console.log("Node with give value not found");
-    }
-
-    sink (parentIndex, childIndex) {
-        let temp = this.queue[parentIndex];
-        this.queue[parentIndex] = this.queue[childIndex];
-        this.queue[childIndex] = temp;
-
-        parentIndex = childIndex;
-         //check if heap is valid
-         let leftChild = 2*parentIndex+1;
-         let rightChild = 2*parentIndex+2;
-         if (leftChild >= this.queue.length) {
-             leftChild = null;
-         }
-         if (rightChild >= this.queue.length) {
-             rightChild = null;
-         }
-         //find out which child to compare with, if any
-         let childToCompare = null;
-         if (leftChild !== null && rightChild !== null) {
-             if (this.queue[leftChild] < this.queue[rightChild]) {
-                 childToCompare = leftChild;
-             } else {
-                 childToCompare = rightChild;
-             }
-         } else if (leftChild === null && rightChild !== null) {
-             childToCompare = rightChild;
-         } else if (leftChild !== null && rightChild === null) {
-             childToCompare = leftChild;
-         } else {
-             //both are null means no child node
-             return true;
-         }
-         //compare with the child node
-         if (this.queue[childToCompare] < this.queue[parentIndex]) {
-             //sink
-             this.sink (parentIndex, childToCompare);
-         }
-         return true;
-    }
-
-    dequeue () {
-        this.delete(this.queue[0]); // just remove the root node
+        return null
     }
 }

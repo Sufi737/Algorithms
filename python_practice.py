88fe8b14af7d47f8fa9ddccd9bf2f03fb0581240
Sufyan <khotsufyan@gmail.com>
2021-07-10 20:15:05 +0530

    Binary search

--- python_practice.py
+++ python_practice.py
@@ -1,43 +1,24 @@
-def constructFenwickTree (custom_list):
-    fenwick_tree=[i for i in custom_list]
-    for index in range(1, len(custom_list)+1):
-        #find the next responsible element
-        nextIndex = index + (index & -index)
-        #update the responsible element
-        if (nextIndex <= len(custom_list)):
-            fenwick_tree[nextIndex-1] += fenwick_tree[index-1]
-
-    return fenwick_tree
-
-def pointUpdate (position, fenwick_tree, toAdd):
-    if (position > len(custom_list)+1):
-        return fenwick_tree
-    fenwick_tree[position-1] += toAdd
-    nextIndex = position + (position & -position)
-    pointUpdate(nextIndex, fenwick_tree, toAdd)
-    return fenwick_tree
+custom_list = [1,2,4,7,8,9]
+target=0
 
 
-def prefixSum(position, fenwick_tree):
-    sum=0
-    while position > 0:
-        sum += fenwick_tree[position-1]
-        position -= (position & -position)
-    return sum
+def binarySearch(custom_list, target):
+    if (target < custom_list[0]) or (target > custom_list[-1]):
+        return None
+    left = 0
+    right = len(custom_list)-1
+    middle = len(custom_list)//2
 
-def rangeQuery (start, end, fenwick_tree):
-    return prefixSum(end, fenwick_tree) - prefixSum(start-1, fenwick_tree)
+    while (left<=right):
+        if (target == custom_list[middle]):
+            break
+        if (target < custom_list[middle]):
+            right = middle
+        else:
+            left = middle+1
 
+        middle = (left+right)//2
+        
+    return middle
 
-custom_list = [3, 4, -2, 7, 3, 11, 5, -8, -9, 2, 4 ,-8]
-fenwick_tree = constructFenwickTree(custom_list)
-
-
-print(fenwick_tree)
-
-updated_tree = pointUpdate(1, fenwick_tree, 5)
-# print(updated_tree)
-
-sum = rangeQuery(3, 7, fenwick_tree)
-print(sum)
-    
+print(binarySearch(custom_list, 7))

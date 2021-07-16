b4f32abe0fdb522787540ad727858a013f12307d
Sufyan <khotsufyan@gmail.com>
2021-07-16 22:46:49 +0530

    Graph Theory

--- python_practice.py
+++ python_practice.py
@@ -1,15 +1,12 @@
-from typing import DefaultDict
-
-
 class Graph:
-    def __init__(self):
-        self.graph = DefaultDict(list)
+    def __init__(self, n):
+        self.graph = [[0 for i in range(n)] for j in range(n)]
 
     def addEdge(self, u, v, w):
-        newEdge = [v, w]
-        self.graph[u].append(newEdge)
+        #considering bidirectional graph
+        self.graph[u][v] = w
 
-newgraph = Graph()
+newgraph = Graph(7)
 newgraph.addEdge(1, 4, 7)
 newgraph.addEdge(1, 3, 3)
 newgraph.addEdge(3, 4, 9)
@@ -17,4 +14,5 @@ newgraph.addEdge(3, 5, 1)
 newgraph.addEdge(2, 5, 2)
 newgraph.addEdge(5, 6, 11)
 print(newgraph.graph)
+
 

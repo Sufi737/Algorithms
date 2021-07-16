0c0633bd432c487299dc3127cd18733a24d21e54
Sufyan <khotsufyan@gmail.com>
2021-07-16 21:36:32 +0530

    Graph Theory

--- python_practice.py
+++ python_practice.py
@@ -1,10 +1,20 @@
-T = int(input())
-while (T>0):
-    n1,n2 = [int(x) for x in input().split()]
-    if n1 > n2:
-        print(">")
-    elif n1 < n2:
-        print("<")
-    else:
-        print("=")
-    T-=1
+from typing import DefaultDict
+
+
+class Graph:
+    def __init__(self):
+        self.graph = DefaultDict(list)
+
+    def addEdge(self, u, v, w):
+        newEdge = [v, w]
+        self.graph[u].append(newEdge)
+
+newgraph = Graph()
+newgraph.addEdge(1, 4, 7)
+newgraph.addEdge(1, 3, 3)
+newgraph.addEdge(3, 4, 9)
+newgraph.addEdge(3, 5, 1)
+newgraph.addEdge(2, 5, 2)
+newgraph.addEdge(5, 6, 11)
+print(newgraph.graph)
+

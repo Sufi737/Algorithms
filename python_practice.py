cc2585b5ba31bccebcfb3090dcbf5a0f3b2ad225
Sufyan <khotsufyan@gmail.com>
2021-07-23 21:16:37 +0530

    Graph Theory

--- python_practice.py
+++ python_practice.py
@@ -1,38 +1,46 @@
-from typing import Counter
-
-
+from collections import defaultdict
+ 
 class Graph:
-    def __init__(self, n):
-        self.graph = [[0 for i in range(n)] for j in range(n)]
-        self.nodeCount = n
+    def __init__(self, vertices):
+        self.graph = defaultdict(list)
+        self.V = vertices 
+ 
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
 
-    def addEdge(self, u, v, w):
-        #considering bidirectional graph
-        self.graph[u-1][v-1] = w
-        self.graph[v-1][u-1] = w
+    def DFS(self, node, stack, visited):
+        #mark the node as visited
+        visited.add(node)
 
-    def BFS(self):
-        visited = [0]*self.nodeCount
-        queue = [0]
-        while queue:
-            current_node = queue.pop()
-            print(current_node+1)
-            visited[current_node] = 1
-            for neighbour in range(0, len(self.graph[0])):
-                if self.graph[current_node][neighbour] > 0 and visited[neighbour]!=1:
-                    queue.append(neighbour)
-                    visited[neighbour] = 1
-            
+        #recursively call the DFS
+        for neighbour in self.graph[node]:
+            if neighbour not in visited:
+                self.DFS(neighbour, stack, visited)
 
-newgraph = Graph(8)
-newgraph.addEdge(1, 4, 7)
-newgraph.addEdge(1, 3, 3)
-newgraph.addEdge(3, 4, 9)
-newgraph.addEdge(3, 5, 1)
-newgraph.addEdge(2, 5, 2)
-newgraph.addEdge(5, 6, 11)
-newgraph.addEdge(7, 8, 3)
 
-print(newgraph.graph)
+        #adding the node to stack at the end of DFS
+        stack.append(node)
 
-newgraph.BFS()
+    def topologicalSort(self):
+        stack = []
+        visited = set()
+
+        for node, edge in list(self.graph.items()):
+            if node not in visited:
+                self.DFS(node, stack, visited)
+                    
+
+        #print the stack
+        while stack:
+            print(stack.pop())
+
+
+graph = Graph(6)
+graph.addEdge(5, 2)
+graph.addEdge(5, 0)
+graph.addEdge(4, 0)
+graph.addEdge(4, 1)
+graph.addEdge(2, 3)
+graph.addEdge(3, 1)
+
+graph.topologicalSort()
